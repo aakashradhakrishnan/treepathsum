b94e04e1d54e206e52662431b244a7870cfe6b30
Aakash Radhakrishnan <Aakash.Radhakrishnan@veritas.com>
2018-12-10 10:57:42 -0800

    adding schema for postgres

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,2 +1,2 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE cities(id serial PRIMARY KEY, firstName VARCHAR(100), lastName VARCHAR(100), password VARCHAR(100));
+DROP TABLE IF EXISTS userlist;
+CREATE TABLE userlist(id serial PRIMARY KEY, first_name VARCHAR(100), last_name VARCHAR(100), password VARCHAR(100), email VARCHAR(100), enabled BOOLEAN DEFAULT TRUE);
